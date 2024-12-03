627679ce7f6a6ffe12c7076e3755b2c68d161283
dannyjiustian <danny.djie72@gmail.com>
2024-12-03 23:23:19 +0700

    feat: #0004 Reimport the used package and remove the not used package

--- api/v1/configs/server.config.js
+++ api/v1/configs/server.config.js
@@ -2,38 +2,38 @@
  * English: imports the modules used
  * Indonesian: mengimpor modul yang digunakan
  */
-import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import fs from "fs";
-import path from "path";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import qrcode from "qrcode";
 import pino from "pino";
-import { responseServer500 } from "./response.config.js";
+import ControllerHandler from "../controllers/handler.controller.js";
+import envConfig from "./env.config.js";
 
 /**
- * English: configure read env
- * Indonesian: konfigurasi baca env
+ * English: read file enviroment
+ * Indonesian: baca file enviroment
  */
-dotenv.config();
-
-/**
- * English: read file enviroment for url
- * Indonesian: baca file enviroment untuk url
- */
-const typeDevelop = process.env.APP_LOCAL || "local";
-const corsOriginServer = process.env.CORS_SERVER_PRODUCTION || "*";
-const baseURL = process.env.BASE_URL_API_VERSION || "v1";
+const typeDevelop = envConfig.get("typeDevelop");
 
 /**
  * English: add-on configuration
  * Indonesian: konfigurasi tambahan
  */
-const portServerConfig = process.env.PORT_SERVER || 3000;
 
 const corsServerConfig = {
-  origin: typeDevelop === "production" ? corsOriginServer : "*",
+  origin: typeDevelop === "production" ? envConfig.get("corsOriginServer") : "*",
+};
+
+const corsSocketConfig = {
+  path: envConfig.get("pathSocket"),
+  transports: ["websocket"],
+  cors: {
+    origin: typeDevelop === "production" ? envConfig.get("corsOriginSocket") : "*",
+    credentials: true,
+  },
 };
 
 /**
@@ -41,19 +41,12 @@ const corsServerConfig = {
  * Indonesian: fungsi-fungsi yang akan digunakan di server
  */
 const notFound = (req, res) => {
-  return responseServer500(res, "Something Wrong!, Check Again!");
-};
-
-const deleteAllFilesInDir = async (dirPath) => {
-  try {
-    const files = await fs.promises.readdir(dirPath);
-    const deleteFilePromises = files.map((file) =>
-      fs.promises.unlink(path.join(dirPath, file))
-    );
-    await Promise.all(deleteFilePromises);
-  } catch (err) {
-    console.log(err);
-  }
+  return ControllerHandler.sendResponse({
+    res,
+    message: "Something wrong at url!, check again!",
+    status: false,
+    code: 500,
+  });
 };
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -74,21 +67,14 @@ serverExpress.use(bodyParser.urlencoded({ extended: false }));
 serverExpress.use(express.json());
 
 /**
+ * English: configure socket.io
+ * Indonesian: konfigurasi socket.io
+ */
+const serverSocket = createServer(serverExpress);
+const io = new Server(serverSocket, corsSocketConfig);
+
+/**
  * English: export configuration
  * Indonesian: export konfigurasi
  */
-export {
-  express,
-  serverExpress,
-  cors,
-  bodyParser,
-  pino,
-  qrcode,
-  baseURL,
-  portServerConfig,
-  corsServerConfig,
-  notFound,
-  fs,
-  deleteAllFilesInDir,
-  delay,
-};
+export { express, serverExpress, serverSocket, io, pino, qrcode, notFound, delay };
